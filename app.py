d625590b7dc6acd4fcc9468189aaeb0a387d9ac0
dbdtndk <leedohan01234@gmail.com>
2026-04-06 15:03:26 +0900

    지도 샘플 찍어보기

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("야호")
-st.write("주원이의 첫 웹사이트")
+st.title("Streamlit 기본 지도")
+
+# 샘플 데이터 생성 (서울 인근)
+df = pd.DataFrame(
+    np.random.randn(100, 2) / [50, 50] + [37.56, 126.97],
+    columns=['lat', 'lon']
+)
+
+st.map(df)
